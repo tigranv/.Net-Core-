accedadc05d0dbeb97644e22a34f5a71fada040d
Tigran Vardanyan <vardanyant.89@gmail.com>
2018-09-12 16:07:33 +0400

    razor pages

---
+++ GettingStartedWebAPI/RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public int Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Genre { get; set; }
+        public int Price { get; set; }
+
+    }
+}
